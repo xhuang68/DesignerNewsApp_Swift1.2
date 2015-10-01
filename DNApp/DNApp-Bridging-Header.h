6590319db09201b9099e80261ec46caab3048bc5
xiao <henryhuang1989@gmail.com>
2015-09-30 21:36:20 -0700

    v0.1

---
+++ DNApp/DNApp-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  DNApp-Bridging-Header.h
+//  DNApp
+//
+//  Created by Xiao Huang on 9/4/15.
+//  Copyright (c) 2015 Xiao Huang. All rights reserved.
+//
+
+#ifndef DNApp_DNApp_Bridging_Header_h
+#define DNApp_DNApp_Bridging_Header_h
+
+#import "MJRefreshAutoNormalFooter.h"
+
+#endif
